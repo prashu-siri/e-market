0fecb087dd6edc28d8f5711e61426bffce69410b
Prashanth Kumar <prashusiri.1990@gmail.com>
2025-07-24 17:01:32 +0530

    Changed the project title

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,7 +7,7 @@ import { Router } from '@angular/router';
 	styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-	title = 'e-commerce';
+	title = 'e-market';
 
 	constructor(private router: Router) {}
 
